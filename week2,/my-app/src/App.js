2932bca07622483db316f2130a8ffcab458e7eab
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-12-30 13:34:03 -0800

    12/30/20

--- week2,/my-app/src/App.js
+++ week2,/my-app/src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import Component from './Component';
+
+
 function App() {
   return (
     <div>
-      <Component title="A" subtitle="blah blah" backgroundColor="Blue" information="blah blah blah"/>
-      <Component title="B" subtitle="" backgroundColor="Red" information=""/>
-      <Component title="C" subtitle="" backgroundColor="Green" information=""/>
-      <Component title="D" subtitle="" backgroundColor="Yellow" information=""/>
-      <Component title="F" subtitle="" backgroundColor="Purple" information=""/>
-      <Component title="E" subtitle="" backgroundColor="Pink" information=""/>
-      <Component title="G" subtitle="" backgroundColor="Orange" information=""/>
-      <Component title="H" subtitle="" backgroundColor="White" information=""/>
-      <Component title="I" subtitle="" backgroundColor="Brown" information=""/>
-      <Component title="J" subtitle="" backgroundColor="Grey" information=""/>
+      <Component title="Dean; " subtitle="Color" backgroundColor="Blue: " information="Dean's Favorite Color"/>
+      <Component title="Sam; " subtitle="Color" backgroundColor="Light Green: " information="Sam's Favorite Color"/>
+      <Component title="Holly; " subtitle="Color" backgroundColor="Pink: " information="Holly's Favorite Color"/>
+      <Component title="Hazel; " subtitle="Color" backgroundColor="Purple: " information="Hazel's Favorite Color"/>
+      <Component title="Adam; " subtitle="Color" backgroundColor="Red: " information="Adam's Favorite Color"/>
+      <Component title="Kevin; " subtitle="Color" backgroundColor="Green: " information="Kevin's Favorite Color"/>
+      <Component title="Nobody; " subtitle="Color" backgroundColor="Yellow: " information="Nobodies Favorite Color"/>
+      <Component title="Somebody; " subtitle="Color" backgroundColor="Orange: " information="Somebodies Favorite Color"/>
+      <Component title="Hair; " subtitle="Color" backgroundColor="Brown: " information="Dean's Hair Color"/>
+      <Component title="Dean2; " subtitle="Color" backgroundColor="Grey: " information="Dean's Second Favorite Color"/>
     </div>
   );
 }
--- week2,/my-app/src/Component.js
+++ week2,/my-app/src/Component.js
@@ -1,11 +1,12 @@
 import React from "react";
+
 function Component(props){
     let title = props.title;
     let backgroundColor = props.backgroundColor;
     let subTitle = props.subTitle;
     let information = props.information;
     return (
-        <h1>Hello</h1>
+        <h1>{title}{backgroundColor}{subTitle}{information}</h1>
     )
 }
 export default Component;
