f7cf452426d3eec390396dee0cfb2f0624fac1b7
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-12-30 00:06:21 -0800

    12/30/20

---
+++ week2,/my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Component from './Component';
+function App() {
+  return (
+    <div>
+      <Component title="A" subtitle="blah blah" backgroundColor="Blue" information="blah blah blah"/>
+      <Component title="B" subtitle="" backgroundColor="Red" information=""/>
+      <Component title="C" subtitle="" backgroundColor="Green" information=""/>
+      <Component title="D" subtitle="" backgroundColor="Yellow" information=""/>
+      <Component title="F" subtitle="" backgroundColor="Purple" information=""/>
+      <Component title="E" subtitle="" backgroundColor="Pink" information=""/>
+      <Component title="G" subtitle="" backgroundColor="Orange" information=""/>
+      <Component title="H" subtitle="" backgroundColor="White" information=""/>
+      <Component title="I" subtitle="" backgroundColor="Brown" information=""/>
+      <Component title="J" subtitle="" backgroundColor="Grey" information=""/>
+    </div>
+  );
+}
+export default App;
---
+++ week2,/my-app/src/Component.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Component(props){
+    let title = props.title;
+    let backgroundColor = props.backgroundColor;
+    let subTitle = props.subTitle;
+    let information = props.information;
+    return (
+        <h1>Hello</h1>
+    )
+}
+export default Component;
