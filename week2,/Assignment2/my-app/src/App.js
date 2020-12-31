fd21d6abd3e1fcc5f01e35444f610503ab6ef9ad
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-12-31 15:39:09 -0800

    12/31/20

--- week2,/Assignment2/my-app/src/App.js
+++ week2,/Assignment2/my-app/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Location from "./Location";
 
 function App() {
+  
   return (
     <div>
       <Location />
--- week2,/Assignment2/my-app/src/Location.js
+++ week2,/Assignment2/my-app/src/Location.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Location() {
+  
   let vacationSpots = [
     {
       place: "Meridian, Idaho",
@@ -28,21 +29,26 @@ function Location() {
       timeToGo: "Spring",
     },
   ];
-
+  
   let vac = vacationSpots.map(function (props) {
     let place = props.place;
     let price = props.price;
     let primeTime = props.timeToGo;
     return (
-      <div>
-        <h1>{place}</h1>
+      <div style={{backgroundColor:"lightgrey"}}>
+        <h1 style={{color:"blue", backgroundColor:"grey"}}>{place}</h1>
         <span>
-          <p>{price}</p>
-          <p>{primeTime}</p>
+          <p style={{color:"black"}}>
+            <b style={{color:"Green"}}>Price:</b> {price}
+          </p>
+          <p style={{color:"Blue"}}>
+            <b style={{color:"Red"}}>Time to go:</b> {primeTime}
+          </p>
         </span>
       </div>
     );
   });
+  
   return(
       <div>{vac}</div>
   )
