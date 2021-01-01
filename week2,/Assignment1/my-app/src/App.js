1cd58376f7aa54a03c971752d73631d98ca77953
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-12-31 19:27:05 -0800

    12/30/20

--- week2,/Assignment1/my-app/src/App.js
+++ week2,/Assignment1/my-app/src/App.js
@@ -5,16 +5,16 @@ import Component from './Component';
 function App() {
   return (
     <div>
-      <Component title="Blue; " subTitle="Color" backgroundColor="#0000FF" information="Dean's Favorite Color"/>
-      <Component title="Grey; " subTitle="Color" backgroundColor="#808080" information="Dean's Second Favorite Color"/>
-      <Component title="Brown; " subTitle="Color" backgroundColor="#A52A2A" information="Dean's Hair Color"/>
-      <Component title="Light Green; " subTitle="Color" backgroundColor="#90EE90" information="Sam's Favorite Color"/>
-      <Component title="Pink; " subTitle="Color" backgroundColor="#FFC0CB" information="Holly's Favorite Color"/>
-      <Component title="Purple; " subTitle="Color" backgroundColor="#800080" information="Hazel's Favorite Color"/>
-      <Component title="Red; " subTitle="Color" backgroundColor="#ff0000" information="Adam's Favorite Color"/>
-      <Component title="Green; " subTitle="Color" backgroundColor="#008000" information="Kevin's Favorite Color"/>
-      <Component title="Orange; " subTitle="Color" backgroundColor="#FFA500" information="Somebodies Favorite Color"/>
-      <Component title="Yellow; " subTitle="Color" backgroundColor="#FFFF00" information="Nobodies Favorite Color"/>
+      <Component title="Blue; " subTitle="Color;" backgroundColor="#0000FF" information=" Dean's Favorite"/>
+      <Component title="Grey; " subTitle="Color;" backgroundColor="#808080" information=" Dean's Second Favorite"/>
+      <Component title="Brown; " subTitle="Color;" backgroundColor="#A52A2A" information=" Dean's Hair"/>
+      <Component title="Light Green; " subTitle="Color;" backgroundColor="#90EE90" information=" Sam's Favorite"/>
+      <Component title="Pink; " subTitle="Color;" backgroundColor="#FFC0CB" information=" Holly's Favorite"/>
+      <Component title="Purple; " subTitle="Color;" backgroundColor="#800080" information=" Hazel's Favorite"/>
+      <Component title="Red; " subTitle="Color;" backgroundColor="#ff0000" information=" Adam's Favorite"/>
+      <Component title="Green; " subTitle="Color;" backgroundColor="#008000" information=" Kevin's Favorite"/>
+      <Component title="Orange; " subTitle="Color;" backgroundColor="#FFA500" information=" Somebodies Favorite"/>
+      <Component title="Yellow; " subTitle="Color;" backgroundColor="#FFFF00" information=" Nobodies Favorite"/>
     </div>
   );
 }
