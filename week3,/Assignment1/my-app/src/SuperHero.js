2998feded32f53ef36aecbed7319d82798c1de72
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-01-07 19:18:03 -0800

    1/7/21

---
+++ week3,/Assignment1/my-app/src/SuperHero.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Data from "./Data";
+
+function Superhero (){
+    let charachters = Data.map(function(props){
+    return(
+        <div>
+            <button onClick={()=>{alert(props.catchPhrase)}}>
+                <h1>{props.name}</h1>
+                <img style={{width:"200px"}}src={props.imageName} alt={"description"}/>
+            </button>
+        </div>
+    )})
+
+    return(
+        <div>{charachters}</div>
+    )
+}
+export default Superhero;
