870b5a48d966271aff833c6b4e2b45109f4a1fee
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-01-21 16:50:53 -0800

    1/21/21

--- week5,/Assignment1/my-app/src/App.js
+++ week5,/Assignment1/my-app/src/App.js
@@ -1,9 +1,39 @@
-import React from 'react'
-/* import { Switch, Route } from "react-router-dom";
- */
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer.js";
+
 function App() {
+  let styles = {
+        textAlign: "center",
+    }
+
   return (
-    <div>
+    <div style={styles}>
+
+      <Navbar /> 
+      
+      <Switch>
+        <Route exact path="/Home">
+          <Home />
+        </Route>
+        <Route exact path="/About">
+          <About />
+        </Route>
+        <Route exact path="/Contact">
+          <Contact />
+        </Route>
+        <Route exact path="/Services">
+          <Services />
+        </Route>
+      </Switch>
+      
+      <Footer />
+
     </div>
   );
 }
---
+++ week5,/Assignment1/my-app/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer(){
+    let styles = {
+        textAlign: "center",
+    }
+    return(
+        <div style={styles}>
+            <footer>Super Awesome footer info</footer>
+        </div>
+    )
+}
+export default Footer;
